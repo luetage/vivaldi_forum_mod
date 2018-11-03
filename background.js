d9e82feb71a7219cb86d9a20615e9ae1b18d4215
Léon McGregor <3817332+LonMcGregor@users.noreply.github.com>
2018-11-03 19:45:27 +0000

    migrating allowing for new buttons

--- background.js
+++ background.js
@@ -8,6 +8,7 @@ chrome.runtime.onMessage.addListener(function(message) {
  * In case item orders change from one instance to next
  * Put them all in order
  * And re-number any not hidden starting from 1
+ * If this update has support for new buttons, mark them as disabled
  */
 function normaliseFormattingToolbarOrders(){
     chrome.storage.sync.get({
@@ -26,6 +27,29 @@ function normaliseFormattingToolbarOrders(){
                 continue;
             }
             settings.formattingToolbar[key] = orderCounter++;
+        }
+        const existingButtons = Object.keys(settings.formattingToolbar);
+        const newButtons = [
+            "bold",
+            "italic",
+            "list",
+            "strikethrough",
+            "link",
+            "picture-o",
+            "zen",
+            "picture",
+            "smile-o",
+            "header",
+            "window-minimize",
+            "quote-right",
+            "code",
+            "file-code-o",
+            "th-large",
+            "list-ol",
+            "shield"
+        ].filter(x => existingButtons.indexOf(x)===-1);
+        for (let index = 0; index < newButtons.length; index++) {
+            settings.formattingToolbar[newButtons[index]] = -1;
         }
         chrome.storage.sync.set({formattingToolbar: settings.formattingToolbar});
     });
