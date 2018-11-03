6cea6332535418c9b97566fc82439e0d78556385
Léon McGregor <3817332+LonMcGregor@users.noreply.github.com>
2018-11-03 19:22:45 +0000

    normalise button orders on install/update

--- background.js
+++ background.js
@@ -3,3 +3,36 @@ chrome.runtime.onMessage.addListener(function(message) {
         chrome.runtime.openOptionsPage();
     }
 });
+
+/**
+ * In case item orders change from one instance to next
+ * Put them all in order
+ * And re-number any not hidden starting from 1
+ */
+function normaliseFormattingToolbarOrders(){
+    chrome.storage.sync.get({
+        formattingToolbar: ""
+    }, settings => {
+        if(settings.formattingToolbar===""){
+            return;
+        }
+        let outOfOrder = Object.entries(settings.formattingToolbar);
+        outOfOrder.sort((a,b) => Number(a[1]) - Number(b[1]));
+        let orderCounter = 1;
+        for (let index = 0; index < outOfOrder.length; index++) {
+            const key = outOfOrder[index][0];
+            const order = outOfOrder[index][1];
+            if(order === -1){
+                continue;
+            }
+            settings.formattingToolbar[key] = orderCounter++;
+        }
+        chrome.storage.sync.set({formattingToolbar: settings.formattingToolbar});
+    });
+}
+
+chrome.runtime.onInstalled.addListener(updateInfo => {
+    if(updateInfo.reason==="update"){
+        normaliseFormattingToolbarOrders();
+    }
+});
