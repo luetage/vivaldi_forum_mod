facc88799da9804ae0e50c51886f6da37bc4938a
luetage <luetage@zoho.com>
2018-11-29 02:59:23 +0100

    standard header removed, version 1.65, options reverted

--- theme.js
+++ theme.js
@@ -75,13 +75,6 @@ function(theme) {
         compactHeader.rel = 'stylesheet';
         document.getElementsByTagName('head')[0].appendChild(compactHeader);
     }
-    else {
-        var standardHeader = document.createElement('link');
-        standardHeader.href = chrome.extension.getURL('mods/standard-header.css');
-        standardHeader.type = 'text/css';
-        standardHeader.rel = 'stylesheet';
-        document.getElementsByTagName('head')[0].appendChild(standardHeader);
-    }
 
     //themes
     if (darkGrey == 1) {
