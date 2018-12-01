fa7ca31cdfb16ac253168aefa35c03dccc265d4b
luetage <luetage@zoho.com>
2018-12-01 09:28:37 +0100

    fixed width font to  inline code (translations)

--- mods/advanced-formatting.js
+++ mods/advanced-formatting.js
@@ -70,7 +70,7 @@ const FORMATTERS = [
 ***
 `, chrome.i18n.getMessage("horizontal_line")],
     ["quote-right", "> ", "", chrome.i18n.getMessage("block_quote")],
-    ["file-code-o", "`", "`", chrome.i18n.getMessage("monospace")],
+    ["file-code-o", "`", "`", chrome.i18n.getMessage("code")],
     ["th-large", `a | a
 ---|---
 x | x
