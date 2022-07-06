40a8276551188f327c2a32ed5eae4a8cb0c56e29
luetage <luetage@vivaldi.net>
2022-07-06 07:45:14 +0200

    fix formatting

--- mods.js
+++ mods.js
@@ -2,28 +2,41 @@
 // Use system emoji
 
 function undoMoji(img) {
-  const rawEmojiSequence = Array.from(img.alt)
+  const rawEmojiSequence = Array.from(img.alt);
   var emojidom = document.createElement("span");
   emojidom.className = "vm-emoji";
   emojidom.title = img.title;
   // adds in joiner character \u200D to correctly stich together emoji sequences
   // the location of joiner is dependent on number of emoji in the sequence
-  let text = ""
+  let text = "";
   const len = rawEmojiSequence.length;
   switch (len) {
     case 2:
       // skin tones break if joined with \u200D, but plain emoji need to be joined
-      if (/light_skin|medium-light_skin|medium_skin|medium-dark_skin|dark_skin/.test(img.title)) {
+      if (
+        /light_skin|medium-light_skin|medium_skin|medium-dark_skin|dark_skin/.test(
+          img.title
+        )
+      ) {
         text = rawEmojiSequence.reduce((prev, curr) => prev + curr);
       } else {
-        text = rawEmojiSequence[0]+"\u200D"+rawEmojiSequence[1];
+        text = rawEmojiSequence[0] + "\u200D" + rawEmojiSequence[1];
       }
       break;
     case 3:
-      text = rawEmojiSequence[0]+rawEmojiSequence[1]+"\u200D"+rawEmojiSequence[2];
+      text =
+        rawEmojiSequence[0] +
+        rawEmojiSequence[1] +
+        "\u200D" +
+        rawEmojiSequence[2];
       break;
     case 4:
-      text = rawEmojiSequence[0]+rawEmojiSequence[1]+"\u200D"+rawEmojiSequence[2]+rawEmojiSequence[3];
+      text =
+        rawEmojiSequence[0] +
+        rawEmojiSequence[1] +
+        "\u200D" +
+        rawEmojiSequence[2] +
+        rawEmojiSequence[3];
       break;
     default:
       text = rawEmojiSequence.reduce((prev, curr) => prev + curr);
@@ -38,28 +51,33 @@ function undoMoji(img) {
 function checkMoji() {
   let allEmoji = Array.from(
     document.querySelectorAll("img.emoji:not(#emoji-dialog img.emoji)")
-  )
-  // emoji indicating sex are always separated into independant img from the base emoji in a sequence
-  // this adds the sex indicator emoji to the previous img's alt text and adjusts the title to match
+  );
+  // emoji indicating sex are always separated into independant img from the
+  // base emoji in a sequence this adds the sex indicator emoji to the previous
+  // img's alt text and adjusts the title to match
   let last = null;
   allEmoji = allEmoji.reduce((prev, curr) => {
-    let next = last ? last.nextSibling ? last.nextSibling : {textContent: "false"} : {textContent: "false"};
+    let next = last
+      ? last.nextSibling
+        ? last.nextSibling
+        : { textContent: "false" }
+      : { textContent: "false" };
     // when an emoji isn't part of a sequence, it doesn't need to be altered
     if (last == null || next.textContent !== "\u200d") {
       prev.push(curr);
     } else {
-      // if an emoji is determined to be the 2nd or greater emoji of a sequence, it needs to be added
-      // back to the 1st emoji of the sequence
-      let previousEmoji = prev[prev.length - 1]
+      // if an emoji is determined to be the 2nd or greater emoji of a sequence,
+      //it needs to be added back to the 1st emoji of the sequence
+      let previousEmoji = prev[prev.length - 1];
       const sex = curr.title.includes("female") ? "woman" : "man";
-      previousEmoji.alt = previousEmoji.alt + curr.alt
-      previousEmoji.title = previousEmoji.title.replace("person", sex)
+      previousEmoji.alt = previousEmoji.alt + curr.alt;
+      previousEmoji.title = previousEmoji.title.replace("person", sex);
       const post = curr.parentElement;
       post.removeChild(curr);
     }
     last = curr;
     return prev;
-  }, [])
+  }, []);
   allEmoji.forEach(undoMoji);
 }
 
