d17575f86203bef19fb910a4b9e7b4069a3173b0
luetage <luetage@vivaldi.net>
2022-12-28 06:51:56 +0100

    introduce css for dark theme

--- options/options.js
+++ options/options.js
@@ -193,7 +193,7 @@ function _restoreSchedule() {
       let schedule = restore.VFM_SCHEDULE;
       let entries = schedule.schedule;
       Object.keys(entries).forEach((entry) => {
-        let defaultTheme = 0;
+        let vivaldiTheme = true;
         const scheduleEntry = document.createElement("div");
         scheduleEntry.id = `schedule-${entry}`;
         scheduleEntry.innerHTML = `<select class="sc-hours"><option value="00">00</option><option value="01">01</option><option value="02">02</option><option value="03">03</option><option value="04">04</option><option value="05">05</option><option value="06">06</option><option value="07">07</option><option value="08">08</option><option value="09">09</option><option value="10">10</option><option value="11">11</option><option value="12">12</option><option value="13">13</option><option value="14">14</option><option value="15">15</option><option value="16">16</option><option value="17">17</option><option value="18">18</option><option value="19">19</option><option value="20">20</option><option value="21">21</option><option value="22">22</option><option value="23">23</option></select><select class="sc-minutes"><option value="00">00</option><option value="05">05</option><option value="10">10</option><option value="15">15</option><option value="20">20</option><option value="25">25</option><option value="30">30</option><option value="35">35</option><option value="40">40</option><option value="45">45</option><option value="50">50</option><option value="55">55</option></select><select class="sc-themes"><option class="vivaldi_light" value="vfm-vivaldi_light">Vivaldi Light</option><option class="vivaldi_dark" value="vfm-vivaldi_dark">Vivaldi Dark</option></select>`;
@@ -216,14 +216,14 @@ function _restoreSchedule() {
           option.value = name;
           if (name === entries[entry].theme) {
             option.setAttribute("selected", true);
-            defaultTheme = 1;
+            vivaldiTheme = false;
           }
           option.text = name.substring(4).replace(/_/g, " ");
           document
             .querySelector(`#schedule-${entry} .sc-themes`)
             .appendChild(option);
         });
-        if (defaultTheme === 0) {
+        if (vivaldiTheme === true) {
           if (entries[entry].theme === "vfm-vivaldi_light") {
             document.querySelector(`#schedule-${entry} .vivaldi_light`).setAttribute("selected", true);
           }
